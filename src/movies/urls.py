e25e186be74a9ed3d2254a67c7d1bb6dc9c535b1
datmemerboi <shravanssd@gmail.com>
2020-12-24 00:23:38 +0530

    Updated handlers

--- src/movies/urls.py
+++ src/movies/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
 	path('new/', views.NewDocHandler.as_view()),
 	path('all/', views.MovieViewHandler.as_view()),
-	path('quick/', views.QuickViewHandler.as_view()),
 	path('retrieve/', views.RetrieveDocHandler.as_view()),
 	path('update/', views.UpdateHandler.as_view()),
 	path('delete/', views.DeleteHandler.as_view()),
-	path('truncate/', views.TruncateHandler.as_view())
+	path('truncate/', views.TruncateHandler.as_view()),
+	# re_path(r'alt/(?P<ID>(\w*|\w*\s)+\s\(\d{4}\))/', views.AltMovieViewHandler.as_view())
 ]
--- src/movies/views.py
+++ src/movies/views.py
@@ -3,23 +3,50 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Movie
 from .serializers import MovieSerializer, QuickMovieSerializer
 
+def ApplyFilterToQuery(query, filters):
+	if not filters or len(filters) < 1:
+		return query
+	for conditionKey, conditionVal in filters:
+		if conditionKey.lower() == 'language':
+			query = query.filter(language = str(conditionVal))
+		# Director filter
+		if conditionKey.lower() == 'director':
+			query = query.filter(director = str(conditionVal))
+		# Cast filter
+		if conditionKey.lower() == 'cast':
+			query = query.filter(cast__icontains = str(conditionVal))
+		# Year filter
+		if conditionKey.lower() == 'year':
+			query = query.filter(year = int(conditionVal))
+		# Keywords filter
+		if conditionKey.lower() == 'keyword':
+			for word in conditionVal:
+				query = query.filter(keyword__icontains = word)
+	return query
+
 class NewDocHandler(APIView):
 	permission_classes = [IsAuthenticated]
-	def CheckMinimalRequirement(self, record):
-		return {'title', 'director', 'year', 'language'}.issubset(record)
+	def MinimalRequirementCheck(self, record):
+		ID = "{} ({})".format(record['short_title'], record['year'])\
+			if 'short_title' in record\
+			else "{} ({})".format(record['title'], record['year'])
 
-	def InsertMovieRecord(self, record):
+		return {'title', 'director', 'year', 'language'}.issubset(record) and not Movie.objects.filter(id = ID).exists()
+
+	def CreateMovieRecord(self, record):
 		ID = "{} ({})".format(record['short_title'], record['year'])\
 			if 'short_title' in record\
 			else "{} ({})".format(record['title'], record['year'])
 		
-		if not Movie.objects.filter(id = ID).exists():
-			Movie.objects.create(id = ID, **record)
+		validator = MovieSerializer(data = { 'id': ID, **record })
+		if validator.is_valid(raise_exception = True):
+			Movie.objects.create(**validator.validated_data)
 		return
 
 	def post(self, request, format = None):
@@ -28,28 +55,33 @@ class NewDocHandler(APIView):
 		else:
 			body = request.data
 		try:
-			if 'multiple' in body and body['multiple']:
-				# Multiple request
+			if 'records' in body:
+				# Multiple record request
 				if len(body['records']) < 1:
 					return Response(status = 400)
 				# Filtering bad records
-				badRecords = list(filter(lambda record: not self.CheckMinimalRequirement(record) , body['records']))
+				badRecords = list(filter(lambda record: not self.MinimalRequirementCheck(record) , body['records']))
 				if len(badRecords) > 0:
 					print("There are {} badRecords".format(len(badRecords)))
 					return Response(status = 400)
 				else:
 					for record in body['records']:
-						self.InsertMovieRecord(record)
+						self.CreateMovieRecord(record)
 			else:
 				# Single record request
-				self.InsertMovieRecord(body)
+				self.CreateMovieRecord(body)
 			return Response(status = 201)
 		
+		except ValidationError:
+			print("Data not valid")
+			return Response(status = 400)
+
 		except IntegrityError:
 			print("Doc with same ID already exists")
 			return Response(status = 409)
 
 		except Exception as err:
+			print(type(err).__name__)
 			print("Exception\n{}".format(err))
 			return Response(status = 500)
 
@@ -60,15 +92,21 @@ class RetrieveDocHandler(APIView):
 		else:
 			body = request.data
 		try:
-			if 'id' not in body:
-				return Response(status = 400)
-			if type(body['id']).__name__ == 'list':
-				queryset = Movie.objects.filter(id__in = body['id'])
-				serializer = MovieSerializer(queryset, many = True)
+			ID = request.query_params.get('id')\
+				if request.query_params.get('id')\
+				else body['id']\
+				if 'id' in body\
+				else None
+			if ID:
+				if type(ID).__name__ == 'list':
+					queryset = Movie.objects.filter(id__in = ID)
+					serializer = MovieSerializer(queryset, many = True)
+				else:
+					queryset = Movie.objects.get(id = ID)
+					serializer = MovieSerializer(queryset)
+				return Response(serializer.data)
 			else:
-				queryset = Movie.objects.get(id = body['id'])
-				serializer = MovieSerializer(queryset)
-			return Response(serializer.data)
+				return Response(status = 400)
 
 		except ObjectDoesNotExist:
 			print("Data not found")
@@ -80,16 +118,29 @@ class RetrieveDocHandler(APIView):
 
 class MovieViewHandler(APIView):
 	def get(self, request):
-		queryset = Movie.objects.all()
-		serializer = MovieSerializer(queryset, many = True)
-		return Response(serializer.data)
+		if type(request.data).__name__ == 'QueryDict':
+			body = request.data.dict()
+		else:
+			body = request.data
+		try:
+			quick = bool(request.query_params.get('quick')) if request.query_params.get('quick') else None
+			limit = int(request.query_params.get('limit')) if request.query_params.get('limit') else None
 
-class QuickViewHandler(APIView):
-	def get(self, request):
-		limit = request.GET.get('limit') if request.GET.get('limit') else 50
-		queryset = Movie.objects.all().order_by('created_at')[:limit]
-		serializer = QuickMovieSerializer(queryset, many = True)
-		return Response(serializer.data)
+			queryset = ApplyFilterToQuery(Movie.objects.all().order_by('-created_at'), body['filter'])\
+				if 'filter' in body\
+				else Movie.objects.all().order_by('-created_at')
+
+			if limit:
+				queryset = queryset[:limit]
+			if quick:
+				serializer = QuickMovieSerializer(queryset, many = True)
+			else:
+				serializer = MovieSerializer(queryset, many = True)
+			return Response(serializer.data)
+
+		except Exception as err:
+			print("Exception\n{}".format(err))
+			return Response(status = 500)
 
 class UpdateHandler(APIView):
 	permission_classes = [IsAuthenticated]
@@ -99,10 +150,16 @@ class UpdateHandler(APIView):
 		else:
 			body = request.data
 		try:
-			if 'id' not in body:
+			ID = request.query_params.get('id')\
+				if request.query_params.get('id')\
+				else body['id']\
+				if 'id' in body\
+				else None
+			if ID:
+				Movie.objects.filter(id = ID).update(**body)
+				return Response(status = 200)
+			else:
 				return Response(status = 400)
-			Movie.objects.filter(id = body['id']).update(**body)
-			return Response(status = 200)
 
 		except ObjectDoesNotExist:
 			print("No data found")
@@ -116,11 +173,17 @@ class DeleteHandler(APIView):
 		else:
 			body = request.data
 		try:
-			if 'id' not in body:
+			ID = request.query_params.get('id')\
+				if request.query_params.get('id')\
+				else body['id']\
+				if 'id' in body\
+				else None
+			if ID:
+				Movie.objects.get(id = body['id']).delete()
+				return Response(status = 200)
+			else:
 				return Response(status = 400)
-			Movie.objects.get(id = body['id']).delete()
-			return Response(status = 200)
-		
+
 		except Exception as err:
 			print("Exception\n{}".format(err))
 			return Response(status = 500)
@@ -131,7 +194,7 @@ class TruncateHandler(APIView):
 		try:
 			Movie.objects.all().delete()
 			return Response(status = 200)
-		
+			
 		except Exception as err:
 			print("Exception\n{}".format(err))
 			return Response(status = 500)
