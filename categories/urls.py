e9f9d25e36189cb7ffded322d201c5ab827429ec
datmemerboi <shravanssd@gmail.com>
2021-02-01 16:10:47 +0530

    Categories module

---
+++ categories/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('new/', views.CreateHandler.as_view()),
+	path('all/', views.ViewHandler.as_view()),
+	path('retrieve/', views.RetrieveHandler.as_view()),
+	path('sync/', views.ResyncHandler.as_view()),
+	path('clear/', views.ClearListHandler.as_view())
+]
