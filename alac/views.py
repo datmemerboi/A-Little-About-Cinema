07af4ad5577f7f28b8fb7cf6862a8838ecb2ce06
datmemerboi <shravanssd@gmail.com>
2021-02-01 16:44:19 +0530

    Modified index

--- alac/views.py
+++ alac/views.py
@@ -1,21 +1,21 @@
 from django.http import JsonResponse
 
 def IndexHandler(request):
-	domain = request.get_host()
+	domain = request.get_raw_uri().strip('/')
 	directions = {
 		"healthy": True,
 		"movies": {
-			"all movies": "http://{}/movie/all".format(domain),
-			"quick movies": "http://{}/movie/all?quick=true".format(domain),
-			"retrieve movie": "http://{}/movie/retrieve/?id=".format(domain),
-			"update movie": "http://{}/movie/update/".format(domain)
+			"all movies": "{}/movie/all".format(domain),
+			"quick movies": "{}/movie/all?quick=true".format(domain),
+			"retrieve movie": "{}/movie/retrieve/?id=".format(domain),
+			"update movie": "{}/movie/update/".format(domain)
 		},
 		"categories": {
-			"all category": "http://{}/category/all/".format(domain),
+			"all category": "{}/category/all/".format(domain),
 			"quick category": "{}/category/all?quick=true".format(domain),
-			"meta category": "http://{}/category/all?meta=true".format(domain),
-			"retrieve category": "http://{}/category/retrieve?id=".format(domain)
+			"meta category": "{}/category/all?meta=true".format(domain),
+			"retrieve category": "{}/category/retrieve?id=".format(domain)
 		},
-		"API documentation": "https://github.com/datmemerboi/A-Little-About-Cinema/API%20Documentation.md"
+		"API documentation": "https://github.com/datmemerboi/A-Little-About-Cinema/blob/main/API%20Documentation.md"
 	}
 	return JsonResponse(directions)
