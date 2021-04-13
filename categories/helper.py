ea9b9af7328ae955d530cb20757a2d6838873c7f
datmemerboi <shravanssd@gmail.com>
2021-04-13 21:42:55 +0530

    Improved condition check in ExtractMeta

--- categories/helper.py
+++ categories/helper.py
@@ -14,23 +14,24 @@ def ExtractMeta(idList, conditions):
 	}
 
 	data = [dict(StatusMovieSerializer(Movie.objects.get(id = ID)).data) for ID in idList]
-	conditionKeys = list(set([con['key'] for con in conditions]))
 
-	meta['directors'] = list(set([record['director'] for record in data])) # Unique directors
+	meta['directors'] = list(set([record['director'][0] for record in data])) # Unique directors
 	meta['languages'] = list(set([record['language'] for record in data])) # Unique languages
 	meta['years'] = list(set([record['year'] for record in data])) # Unique years
 
-	if "director" in conditionKeys:
-		# "director" is a condition
-		# Cannot filter for directors
-		del meta['directors']
-	if "language" in conditionKeys:
-		# "language" is a condition
-		# Cannot filter for languages
-		del meta['languages']
-	if "year" in conditionKeys:
-		# "year" is a condition
-		# Cannot filter for years
-		del meta['years']
+	for con in conditions:
+		key = con['key']
+		opr = con['operator']
+		val = con['value']
+		if key == "director" and (opr == "in" or copr == "contains") and len(val) == 1:
+			# All records have the same director
+			del meta['directors']
+		if key == "language" and (opr == "is" or opr == "equals"):
+			# All records have the same language
+			del meta['languages']
+		if key == "year" and (opr == "is" or opr == "equals"):			
+			# All records have the same year
+			del meta['years']
+
 	meta['recommended'] = len(list(filter(lambda record: record['status'] > 0, data)))
 	return meta
