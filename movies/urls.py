2617d38c143f58d45abdc1628d2f531e273c2b05
datmemerboi <shravanssd@gmail.com>
2021-04-14 21:37:24 +0530

    Removed unused url

--- movies/urls.py
+++ movies/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
 	path('retrieve/', views.RetrieveHandler.as_view()),
 	path('update/', views.UpdateHandler.as_view()),
 	path('delete/', views.DeleteHandler.as_view()),
-	path('truncate/', views.TruncateHandler.as_view()),
-	# re_path(r'alt/(?P<ID>(\w*|\w*\s)+\s\(\d{4}\))/', views.AltViewHandler.as_view())
+	path('truncate/', views.TruncateHandler.as_view())
 ]
